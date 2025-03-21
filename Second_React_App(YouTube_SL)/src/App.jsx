8a70f5963435c9516fd0eb22eeb545f5fbadccf6
Hashan Madhuranga Dissanayaka <141218317+HashanMadhu@users.noreply.github.com>
2025-03-21 23:37:50 +0530

    Setup and create Component
    Create the "NewOne " components for study about the Components of the React

---
+++ Second_React_App(YouTube_SL)/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NewOne from './Components/NewOne.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <NewOne />
+    </>
+  )
+}
+
+export default App
