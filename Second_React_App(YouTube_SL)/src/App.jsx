b4e6419694adbe8b7b889269574c09eb7bf55eb8
Hashan Madhuranga Dissanayaka <141218317+HashanMadhu@users.noreply.github.com>
2025-03-25 00:43:32 +0530

    routing

--- Second_React_App(YouTube_SL)/src/App.jsx
+++ Second_React_App(YouTube_SL)/src/App.jsx
@@ -4,14 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import NewOne from './Components/NewOne.jsx'
 import Home from './Pages/Home.jsx'
+import {BrowserRouter as Router,Route,Switch, BrowserRouter, Routes} from 'react-router-dom'; //install the Routing package
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Home/>
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element = {<Home/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
