3e3e8c36a51b5ffcd52fdf3e6dd91ea3bbffc578
Hashan Madhuranga Dissanayaka <141218317+HashanMadhu@users.noreply.github.com>
2025-03-25 19:12:00 +0530

    Finish the routing
    Routing the about and contact page

--- Second_React_App(YouTube_SL)/src/App.jsx
+++ Second_React_App(YouTube_SL)/src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import NewOne from './Components/NewOne.jsx'
 import Home from './Pages/Home.jsx'
+import About from './Pages/About.jsx'
+import Contact from './Pages/Contact.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'; //install the Routing package
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} ></Route>
+          <Route path='/about' element={<About />} ></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
